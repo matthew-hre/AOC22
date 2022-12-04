478ef2d0a7655b1159220bf846251e07330baa67
matthew-hre <whycardboard@gmail.com>
2022-12-03 22:15:18 -0700

    day 4 finished

--- day_04.py
+++ day_04.py
@@ -4,15 +4,26 @@ from template import Template
 class Day_04(Template):
     def __init__(self):
         self.day = 4
-        super().__init__(self.day)
+        super().__init__(self.day, "test.txt")
 
-        self.set_data(self.get_data())
+        self.set_data([[list(map(int, y.split("-"))) for y in x.split(",")]
+                      for x in self.get_data().split()])
 
     def part1(self):
-        pass
+        count = 0
+        for first, second in self.get_data():
+            count += 1 if (first[0] <= second[0] and first[1] >= second[1]
+                           ) or (second[0] <= first[0] and second[1] >= first[1]) else 0
+
+        return count
 
     def part2(self):
-        pass
+        count = 0
+        for first, second in self.get_data():
+            count += 1 if bool(set(range(first[0], first[1] + 1))
+                               & set(range(second[0], second[1] + 1))) else 0
+
+        return count
 
 
 print(Day_04())
